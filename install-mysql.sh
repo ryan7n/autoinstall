71aae898f9afbad108e00184404bbc299b2aea98
wyy0117 <349093330@qq.com>
2019-01-23 15:53:35 +0800

    fix install mysql.

--- install-mysql.sh
+++ install-mysql.sh
@@ -12,7 +12,7 @@ character-set-server=utf8
 lower_case_table_names=1
 
 skip-host-cache
-skip-name-resolve">~/docker/data/mysql5.7/conf
+skip-name-resolve">~/docker/data/mysql5.7/conf/confg-file.cnf
 
 echo "start mysql"
 docker run --name mysql -p 3306:3306 -v ~/docker/data/mysql5.7/conf:/etc/mysql/conf.d -v ~/docker/data/mysql5.7/dbdata:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=root -d mysql:5.7
