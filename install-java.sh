8db7ac1587386f8ecc7104f2ecb5958a68e867ca
wyy0117 <349093330@qq.com>
2019-05-19 10:58:50 +0800

    optimize.

--- install-java.sh
+++ install-java.sh
@@ -7,11 +7,10 @@ source "$HOME/.sdkman/bin/sdkman-init.sh"
 
 echo "cp zip"
 mkdir -p ~/.sdkman/archives
-cp grails-3.3.0.zip groovy-2.4.15.zip java-8.0.201-oracle.zip 
-~/.sdkman/archives
+cp grails-3.3.0.zip groovy-2.4.15.zip ~/.sdkman/archives
 
 echo "install java"
-sdk i java 8.0.201-oracle
+sdk i java 8.0.212-zulu
 
 echo "install groovy"
 sdk i groovy 2.4.15
--- install-liferay.sh
+++ install-liferay.sh
@@ -1,27 +1,28 @@
+TOMCAT_HOME=~/tomcat
 echo "unzip liferay tomcat"
-mkdir -p ~/tomcat
-unzip liferay*.zip -d ~/tomcat/
+mkdir -p $TOMCAT_HOME
+unzip liferay*.zip -d $TOMCAT_HOME/
 
 echo "cp bimrun1"
-cp -r ~/tomcat/liferay-portal-6.2-ce-ga6 ~/tomcat/liferay-portal-6.2-bimrun1
+cp -r $TOMCAT_HOME/liferay-portal-6.2-ce-ga6 $TOMCAT_HOME/liferay-portal-6.2-bimrun1
 
 echo "cp property file"
-cp portal-ext.properties ~/tomcat/liferay-portal-6.2-bimrun1/
+cp portal-ext.properties $TOMCAT_HOME/liferay-portal-6.2-bimrun1/
 
 echo "cp log config file"
-cp -r META-INF/ ~/tomcat/liferay-portal-6.2-bimrun1/tomcat-7.0.62/lib/
+cp -r META-INF/ $TOMCAT_HOME/liferay-portal-6.2-bimrun1/tomcat-7.0.62/lib/
 
 echo "cp setenv.sh"
-cp setenv.sh ~/tomcat/liferay-portal-6.2-bimrun1/tomcat-7.0.62/bin
+cp setenv.sh $TOMCAT_HOME/liferay-portal-6.2-bimrun1/tomcat-7.0.62/bin
 
 echo "cp bimrun2"
-cp -r ~/tomcat/liferay-portal-6.2-ce-ga6 ~/tomcat/liferay-portal-6.2-bimrun2
+cp -r $TOMCAT_HOME/liferay-portal-6.2-ce-ga6 $TOMCAT_HOME/liferay-portal-6.2-bimrun2
 
 echo "cp property file"
-cp portal-ext.properties ~/tomcat/liferay-portal-6.2-bimrun2/
+cp portal-ext.properties $TOMCAT_HOME/liferay-portal-6.2-bimrun2/
 
 echo "cp log config file"
-cp -r META-INF/ ~/tomcat/liferay-portal-6.2-bimrun2/tomcat-7.0.62/lib/
+cp -r META-INF/ $TOMCAT_HOME/liferay-portal-6.2-bimrun2/tomcat-7.0.62/lib/
 
 echo "cp setenv.sh"
-cp setenv.sh ~/tomcat/liferay-portal-6.2-bimrun2/tomcat-7.0.62/bin
+cp setenv.sh $TOMCAT_HOME/liferay-portal-6.2-bimrun2/tomcat-7.0.62/bin
